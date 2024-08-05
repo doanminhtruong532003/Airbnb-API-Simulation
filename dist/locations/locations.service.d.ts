cbb838e20e8e77f1489813cac89fdf74ed0ae581
Doan Minh Truong <doanminhtruong53+github@gmail.com>
2024-08-06 01:00:37 +0700

    Initial commit

---
+++ dist/locations/locations.service.d.ts
@@ -0,0 +1,9 @@
+import { LocationUpdateDTO, LocationDTO } from './dto';
+export declare class LocationsService {
+    private readonly prisma;
+    findAll(searchKeyword: string, records: number, page: number): Promise<LocationDTO[]>;
+    findOne(id: number): Promise<LocationDTO>;
+    create(data: LocationDTO): Promise<LocationDTO>;
+    update(id: number, data: LocationUpdateDTO): Promise<LocationDTO>;
+    delete(id: number): Promise<LocationDTO>;
+}
---
+++ dist/users/users.controller.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="multer" />
+import { UsersService } from './users.service';
+import { UserDTO, UserUpdateDTO } from './dto';
+export declare class UsersController {
+    private readonly usersService;
+    constructor(usersService: UsersService);
+    get(id: string, searchKeyword: string, records: string, page: string): Promise<{
+        message: string;
+        data: any;
+    }>;
+    create(data: UserDTO, avatar: Express.Multer.File): Promise<{
+        message: string;
+        data: (role?: string) => Record<string, any>;
+    }>;
+    update(id: string, data: UserUpdateDTO, avatar: Express.Multer.File): Promise<{
+        message: string;
+        data: Record<string, any>;
+    }>;
+    delete(id: number): Promise<{
+        message: string;
+    }>;
+}
